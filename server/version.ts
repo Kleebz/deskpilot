74dc978232786429f28ccf0202f8d4316119ace5
Jacob Morosoff <jacobmorosoff@gmail.com>
2026-09-12 16:53:59 -0700

    Release 0.1.9

--- server/version.ts
+++ server/version.ts
@@ -11,7 +11,7 @@
 // baked-in value takes precedence.
 import { COMMIT as BAKED } from "./build-info.ts";
 
-export const VERSION = "0.1.8";
+export const VERSION = "0.1.9";
 
 function fromGit(root: string): string {
   try {
