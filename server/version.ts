32a087a6c8a23a1f7c329f9ddf517645d02eb5f6
Jacob Morosoff <jacob.morosoff@gmail.com>
2026-09-14 18:48:46 -0700

    Bump version to 0.1.15

--- server/version.ts
+++ server/version.ts
@@ -11,7 +11,7 @@
 // baked-in value takes precedence.
 import { COMMIT as BAKED } from "./build-info.ts";
 
-export const VERSION = "0.1.14";
+export const VERSION = "0.1.15";
 
 function fromGit(root: string): string {
   try {
